689498d949b2b0cb46bc49b5038d8f5628fbcb59
Gerson Cano Meneses <gerson.cano210@pascualbravo.edu.co>
2025-11-04 23:47:32 -0500

    lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                                   # Nombre del paquete
+    version="0.1.0",                                     # Versión inicial
+    packages=find_packages(),                            # Paquetes a incluir
+    description="Un paquete pip simple de saludo",       # Breve descripción
+    author="Gerson Cano Meneses",                        # Tu nombre
+    author_email="gerson.cano210@pascualbravo.edu.co",   # Tu correo electrónico
+    url="https://github.com/gersoncano210/tercerrepo",   # URL del proyecto
+)
